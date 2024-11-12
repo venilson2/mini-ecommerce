b260c48bbe5a1e42c7ea5913aba508967ff53c8e
venilson2 <venilson2009@hotmail.com>
2024-11-12 02:58:08 -0300

    refactor: remove comements

--- database/seeders/UserSeeder.ts
+++ database/seeders/UserSeeder.ts
@@ -1,8 +1,6 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import User from 'App/Models/User'
 import Hash from '@ioc:Adonis/Core/Hash'
-import { DateTime } from 'luxon'
-import { v4 as uuid } from 'uuid'
 
 export default class UserSeederSeeder extends BaseSeeder {
   public async run () {
@@ -10,8 +8,6 @@ export default class UserSeederSeeder extends BaseSeeder {
       fullName: 'Example User',
       email: 'example@example.com',
       password: await Hash.make('123'),
-      // createdAt: DateTime.local(),
-      // updatedAt:  DateTime.local(),
     })
   }
 }
